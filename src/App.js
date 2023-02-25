be4dc4b8ac4dd68574363ee609f921673c5cca0f
sadvika7reddy <sadvika7reddy>
2023-02-25 14:57:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Signup from "./component/Signup";
+
+function App() {
+  return (
+    <div>
+      <Signup/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Signup.js
@@ -0,0 +1,66 @@
+import {Fragment,useRef,useState,useContext} from 'react';
+import classes from './Signup.module.css';
+const Signup=()=>{
+    const emailInputRef=useRef();
+    const passwordInputRef=useRef();
+    const EventHandler=(event)=>{
+        event.preventDefault();
+        const enteredEmail=emailInputRef.current.value;
+        const enteredPassword=passwordInputRef.current.value;
+      localStorage.setItem('email',enteredEmail);
+      
+      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCewUWbgztJYjhsb5UEmf3Ni6T_ehpNmXQ',{
+            method:'POST',
+            body:JSON.stringify(
+              {
+                email:enteredEmail,
+                password:enteredPassword,
+                returnSecureToken:true
+              }
+            ),
+            headers :{
+              'Content-Type':'application/json'
+            }
+          }
+          ).then(res=>{
+            if(res.ok){
+                
+              return res.json();
+              
+    
+            }
+            else{
+              return res.json().then((data)=>{
+                let errorMessage='Authentication Failed';
+                throw new Error(errorMessage);
+              })
+            }
+        }).then((data)=>{
+            console.log(data)
+    
+          })
+          .catch((err)=>{
+            alert(err.message)
+          })
+    } 
+   
+    return(
+        <Fragment>
+            <form className={classes.auth} onSubmit={EventHandler}>
+                <div>
+                    <label>Email</label>
+                    <input type='email' required ref={emailInputRef}/>
+                    <br/>
+                    &nbsp;
+                    </div>
+                    <div>
+                    <label>Password</label>
+                    <input type='password' required ref={passwordInputRef}/><br/>
+                    &nbsp;
+                    </div>
+                    <button type='submit'>Login</button>
+            </form> 
+        </Fragment>
+    )
+}
+export default Signup;
