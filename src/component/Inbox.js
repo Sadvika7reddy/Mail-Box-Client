d8dd1be310a03d6729d6ff4170421e6b7415c671
sadvika7reddy <sadvika7reddy>
2023-03-02 12:28:39 +0530

    delete

--- src/component/Inbox.js
+++ src/component/Inbox.js
@@ -27,6 +27,14 @@ const Inbox=()=>{
         dispatch(formAction.close(id))
 
       }
+      const deleteHandler=async (id)=>{
+        const userEmail=localStorage.getItem('email');
+        const user=userEmail.replace('.','q');
+        const users=user.replace('@','s')
+        
+      let res=await axios.delete(`https://mailbox-31eb0-default-rtdb.firebaseio.com/${users}/${id}.json`) 
+    }
+
     return( 
         <div>
             <ul>
@@ -37,6 +45,8 @@ const Inbox=()=>{
                             <Link to={`/inbox/${user.id}`}>
                                 <button onClick={()=>changeHandler(user.id)}>{user.title}</button>
                                 </Link></h2>
+                                <button type='submit' style={{background: "Tomato"}} onClick={()=>{deleteHandler(user.id)}}>Delete</button>
+        
                     </li>
                 ))}
             </ul>
